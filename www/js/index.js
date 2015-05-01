8b369bd6355bb2c1406c72cdc91d499fd08f6fa1
arthuroconh <arthuro_b@outlook.com>
2015-05-01 17:58:17 -0430

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -59,17 +59,6 @@ var app = {
     // result contains any message sent from the plugin call 
     successHandler: function(result) { 
         alert('Callback Success! Result = '+result) 
-                            var valor={"dato":2}; 
-                    $.ajax({ 
-                            url:'http://desarrollotricolor.com.ve/PhonegapPushNotifications/registro.php', 
-                            type:'post', 
-                            data:valor,
-                            dataType:'json', 
-                            error:function(jqXHR,text_status,strError){}, 
-                            success:function(){
-                                alert('Registrado');
-                            }
-                        });
     }, 
     errorHandler:function(error) { 
         alert(error); 
@@ -81,26 +70,15 @@ var app = {
                 if ( e.regid.length > 0 ) 
                 { 
                     console.log("Regid " + e.regid); 
-                    alert('REGISTRO= '+e.regid); 
-                    //Cuando se registre enviamos la variable a ajax
-                            var valor={"dato":3}; 
-                    $.ajax({ 
-                            url:'http://desarrollotricolor.com.ve/PhonegapPushNotifications/registro.php', 
-                            type:'post', 
-                            data:valor,
-                            dataType:'json', 
-                            error:function(jqXHR,text_status,strError){}, 
-                            success:function(){
-                                alert('Registrado');
-                            }
-                        });
-                }
-                 
+                    alert('registration id = '+e.regid); 
+                    //Cuando se registre le pasamos el regid al input 
+                    document.getElementById('regId').value = e.regid;
+                } 
             break; 
 
             case 'message': 
               // NOTIFICACION!!! 
-              alert('Neva Notifiacion : '+e.message); 
+              alert('Neva Notifiacion : '+e.message+''); 
             break; 
 
             case 'error': 
