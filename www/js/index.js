3166c04146fef951e6b9aab75a98e37e55c1f47f
arthuroconh <arthuro_b@outlook.com>
2015-05-01 16:43:20 -0430

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -70,7 +70,7 @@ var app = {
                 if ( e.regid.length > 0 ) 
                 { 
                     console.log("Regid " + e.regid); 
-                    alert('registration id = '+e.regid); 
+                    alert('REGISTRO = '+e.regid); 
                     //Cuando se registre le pasamos el regid al input 
                     var dato= e.regid; 
                     $.ajax({ 
